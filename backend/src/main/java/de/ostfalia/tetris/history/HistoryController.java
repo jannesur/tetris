75d7a9ba3000c91ff51c52657129d4a5bac344c4
jannesurborg <janne.surborg@hotmail.de>
2025-11-09 11:50:51 +0100

    controller für history hinzugefügt

--- backend/src/main/java/de/ostfalia/tetris/history/HistoryController.java
+++ backend/src/main/java/de/ostfalia/tetris/history/HistoryController.java
@@ -1,5 +1,42 @@
 package de.ostfalia.tetris.history;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.ostfalia.tetris.player.Player;
+import de.ostfalia.tetris.player.PlayerService;
+
+@RestController
+@RequestMapping("/history")
 public class HistoryController {
+
+    private final HistoryService historyService;
+
+	public HistoryController(HistoryService historyService) {
+		this.historyService = historyService;
+	}
+
+    @GetMapping
+    public List<History> getAllHistories() {
+        return this.historyService.getAllHistories();
+    }
+
+    @GetMapping("/{id}")
+    public History getHistoryById(@PathVariable Long id) {
+        return this.historyService.getHistoryById(id);
+    }
     
+
+	@PostMapping
+	public History createHistory(@RequestBody History history) {
+		return this.historyService.createHistory(history);
+	}
+	
 }
