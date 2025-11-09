3d3120d83c493788cefee2086fc3c0fa9af5d3c9
jannesurborg <janne.surborg@hotmail.de>
2025-11-09 11:39:16 +0100

    controller für player hinzugefügt

--- backend/src/main/java/de/ostfalia/tetris/player/PlayerController.java
+++ backend/src/main/java/de/ostfalia/tetris/player/PlayerController.java
@@ -1,5 +1,53 @@
 package de.ostfalia.tetris.player;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/player")
 public class PlayerController {
     
+    private final PlayerService playerService;
+
+    public PlayerController (PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return this.playerService.getAllPlayers();
+    }
+
+    @GetMapping("/{id}")
+    public Player getPlayerById(@PathVariable Long id) {
+        return this.playerService.getPlayerById(id);
+    }
+    
+
+	@PostMapping
+	public Player createPlayer(@RequestBody Player player) {
+		return this.playerService.createPlayer(player);
+	}
+
+	@PutMapping("/{id}")
+	public Player updatePlayer(@PathVariable Long id, @RequestBody Player player) {
+		return this.playerService.updatePlayer(id, player);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deletePlayer(@PathVariable Long id) { 
+    }    
+		
 }
