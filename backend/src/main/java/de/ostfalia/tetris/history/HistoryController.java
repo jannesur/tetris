058e45b460bca7fdd9883d942b3aa5c68c7c8d1c
jannesurborg <janne.surborg@hotmail.de>
2025-11-09 11:59:47 +0100

    service für history hinzugefügt

--- backend/src/main/java/de/ostfalia/tetris/history/HistoryController.java
+++ backend/src/main/java/de/ostfalia/tetris/history/HistoryController.java
@@ -1,17 +1,12 @@
 package de.ostfalia.tetris.history;
 
 import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import de.ostfalia.tetris.player.Player;
-import de.ostfalia.tetris.player.PlayerService;
 
 @RestController
 @RequestMapping("/history")
--- backend/src/main/java/de/ostfalia/tetris/history/HistoryService.java
+++ backend/src/main/java/de/ostfalia/tetris/history/HistoryService.java
@@ -1,5 +1,30 @@
 package de.ostfalia.tetris.history;
 
+import java.util.List;
+import jakarta.persistence.EntityNotFoundException;
+
 public class HistoryService {
-    
+
+    private final HistoryRepository historyRepository;
+
+    public HistoryService(HistoryRepository historyRepository) {
+        this.historyRepository = historyRepository;
+    }
+
+    public List<History> getAllHistories() {
+        return this.historyRepository.findAll();
+    }
+
+    public History getHistoryById(Long id) {
+        if(!this.historyRepository.existsById(id)) {
+            throw new EntityNotFoundException("no history with given id found");
+        }
+        return this.historyRepository.findById(id).get();
+    }
+
+    public History createHistory(History history) {
+        return this.historyRepository.save(history);
+    }
+
+ 
 }
