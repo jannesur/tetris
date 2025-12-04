a33e14c1761b9e34064a7401833815babcb7b12d
Konstantin Königshofen <koenigshofn11ko@gmail.com>
2025-12-04 14:34:36 +0100

    Test-Commit

--- frontend/src/main/java/org/example/tetrisprototyp/GameEngine/Settings.java
+++ frontend/src/main/java/org/example/tetrisprototyp/GameEngine/Settings.java
@@ -1,5 +1,7 @@
 package org.example.tetrisprototyp.GameEngine;
 
+
+// Speichert den Schwierigkeitsgrad des Spiels, damit dieser von dem GameController verwendet werden kann.
 public class Settings {
     private static int difficulty = 1;
     public static int getDifficulty() { return difficulty; }
